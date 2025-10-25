bccd31c303d73cf1caa242c204f6c1c5ee284f0a
chinibaba6 <chinibaba6@github.com>
2025-10-25 09:16:31 +0600

    Add absolute minimal test plugins for diagnostics

---
+++ src/index-minimal.tsx
@@ -0,0 +1,6 @@
+// Absolute minimal plugin - if this doesn't work, nothing will
+export default {
+  onLoad: () => console.log("✅ MINIMAL PLUGIN LOADED!"),
+  onUnload: () => console.log("❌ MINIMAL PLUGIN UNLOADED")
+};
+
